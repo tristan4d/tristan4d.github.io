5e7235510093c30d0a40fc11cfb76094ee37a272
Tristan Ford <tristan4d@gmail.com>
2023-08-20 17:11:47 -0700

    fix: home image

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -43,7 +43,7 @@ export default function Home({ min_500 }) {
                     borderRadius: 2
                 }}
                 alt="Me on a boat."
-                src="./assets/images/home/cover-image.JPG"
+                src="/assets/images/home/cover-image.JPG"
             />
         </Box>
     )
